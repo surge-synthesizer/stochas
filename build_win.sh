4a6a0446bcaba46fd78fe77282a0c75f86d84e38
Andrew Shakinovsky <andrew.shakinovsky@sas.com>
2020-07-21 16:21:02 -0400

    version tweaks

--- build_win.sh
+++ build_win.sh
@@ -8,6 +8,7 @@
 #export INNO="/c/Program\ Files\ \(x86\)/Inno\ Setup\ 5/Compil32.exe"
 export INNO="/c/Program Files (x86)/Inno Setup 5/iscc.exe"
 export VST2_PATH=/Z/dev/VST_SDK/VST2_SDK
+export STOCHAS_VERSION=`cat VERSION`
 cmake -B build32 -DCMAKE_INSTALL_PREFIX=/c/data/juce6_install
 cmake --build build32 --config Release
 cmake -B build -A x64 -DCMAKE_INSTALL_PREFIX=/c/data/juce6_install

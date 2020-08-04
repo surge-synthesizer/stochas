aec9a25c478eb9aaa84d79e85166c16ee684f348
Andrew Shakinovsky <luvshak@gmail.com>
2020-08-04 17:00:57 -0400

    more automation on mac build

--- build_mac.sh
+++ build_mac.sh
@@ -1,9 +1,12 @@
 #
 # This will build the mac install and disk image.
+# You will need to have JUCE install at ../JUCE_install relative to projects root dir
 # you will need to have "Packages 1.2.9" installed on your system for this to work correctly
 # Be sure to run this from the project's root directory
 export STOCHAS_VERSION=`cat VERSION`
-cmake -B build
+export JUCEINST=`pwd`/../JUCE_install
+#cmake -B build
+cmake -B build -DCMAKE_INSTALL_PREFIX=${JUCEINST}
 cmake --build build --config Release
 packagesbuild "install/mac/Stochas Open.pkgproj"
 hdiutil create -volname "Stochas ${STOCHAS_VERSION}" -srcfolder build.install/mac/stochas_open -ov -format UDRO build.install/mac/stochas.${STOCHAS_VERSION}.dmg

cde8cfa96dcba778527b3d5c279306dd9036744b
Andrew Shakinovsky <andrew@binarydog.com>
2020-08-08 16:52:03 -0400

    allow specifying is synth and is effect on cmd line for cmake. fix cmake script for vst2 check

--- build_mac.sh
+++ build_mac.sh
@@ -3,7 +3,7 @@
 # you will need to have "Packages 1.2.9" installed on your system for this to work correctly
 # Be sure to run this from the project's root directory
 STOCHAS_VERSION=`cat VERSION`
-cmake -B build -DSTOCHAS_VERSION=${STOCHAS_VERSION}
+cmake -B build -DSTOCHAS_VERSION=${STOCHAS_VERSION} -DSTOCHAS_IS_SYNTH=TRUE -DSTOCHAS_IS_MIDI_EFFECT=TRUE
 cmake --build build --config Release
 retVal=$?
 if [ $retVal -eq 0 ]; then

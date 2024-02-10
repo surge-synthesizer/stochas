0cb2e838808debf866fdf00fe1566068061c78ca
Andrew Shakinovsky <andrew@binarydog.com>
2024-02-09 22:53:05 -0500

    cleanup (#140)
    * remove commented
    
    * update build containers to latest

--- src/StochaEngine.cpp
+++ src/StochaEngine.cpp
@@ -628,8 +628,7 @@ void StochaEngine::setAutomationParameterValue(int paramId, int value)
       case SEQ_AUT_POLY_BIAS: targ = &mOverridePolyBias; break;
       case SEQ_AUT_CURRENT_PATTERN: targ = &mOverridePattern; break;
       case SEQ_AUT_TRANSPOSE: targ = &mOverrideTranspose; break;
-      case SEQ_AUT_GLOBAL_SWING: targ = &mOverrideSwing; break;
-      //case SEQ_AUT_RECORD: targ =
+      case SEQ_AUT_GLOBAL_SWING: targ = &mOverrideSwing; break;      
       default: break;
       }
       if (targ)

438fe712a0320f4968e63c7cded6a0bf61da939b
Paul Walker <paul@pwjw.com>
2024-08-21 14:32:29 -0400

    Towards github actions, step 2
    Move the azure build for linux into a script also.

---
+++ scripts/linux-build.sh
@@ -0,0 +1,11 @@
+cmake -Bbuild -DSTOCHAS_VERSION=${STOCHAS_VERSION}
+cmake --build ignore/build --config Release
+LINARCH=`uname -m`
+GH=`git log -1 --format=%h`
+NM=stochas-${STOCHAS_VERSION}.${GH}.linux.tgz
+mkdir -p ignore/build/product/
+mkdir -p ignore/build/Stochas/Standalone
+cp -r ignore/build/stochas_artefacts/VST3/* build/Stochas
+cp -r ignore/build/stochas_artefacts/CLAP/* build/Stochas
+cp -r ignore/build/stochas_artefacts/Standalone/* build/Stochas/Standalone
+tar cvzf "build/product/${NM}" -C build Stochas

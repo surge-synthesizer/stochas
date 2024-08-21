22c453627654c708b512d8c777b569e3609ed5b2
Paul Walker <paul@pwjw.com>
2024-08-21 14:40:52 -0400

    fff

--- scripts/linux-build.sh
+++ scripts/linux-build.sh
@@ -1,8 +1,8 @@
-cmake -Bbuild -DSTOCHAS_VERSION=${STOCHAS_VERSION}
+cmake -Bignore/build -DSTOCHAS_VERSION=${STOCHAS_VERSION}
 cmake --build ignore/build --config Release
 LINARCH=`uname -m`
 GH=`git log -1 --format=%h`
-NM=stochas-${STOCHAS_VERSION}.${GH}.linux.tgz
+NM=stochas-${STOCHAS_VERSION}.${GH}.linux-${LINARCH}.tgz
 mkdir -p ignore/build/product/
 mkdir -p ignore/build/Stochas/Standalone
 cp -r ignore/build/stochas_artefacts/VST3/* build/Stochas

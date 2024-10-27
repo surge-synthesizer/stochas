c133498b1cc539eeed73d49d9d21d0dacbbe6698
Paul <baconpaul@users.noreply.github.com>
2024-10-27 09:43:29 -0400

    Move windows azure dev build to actions (#157)
    Windows PR action has a script which builds the x64 installer version

---
+++ scripts/win-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -ex
+# 64 bit
+cmake -Bbuild -A x64 -DSTOCHAS_VERSION=${STOCHAS_VERSION}
+cmake --build build --config Release
+# 32 bit
+mkdir -p build/product
+mkdir -p zip/x64
+cp -r build/stochas_artefacts/Release/VST3/* zip/x64
+cp -r build/stochas_artefacts/Release/CLAP/* zip/x64
+cp -r build/stochas_artefacts/Release/Standalone/* zip/x64
+NM=stochas-${STOCHAS_VERSION}.${GH}-win.zip
+# see what we have
+powershell Compress-Archive -DestinationPath "build/product/${NM}" -Force -Path "zip"
+ls build/product
+
+nuget install innosetup
+mkdir -p build/product/
+iscc //Obuild\\product //Fstochas_windows_installer //DSTOCHAS_VERSION=${STOCHAS_VERSION}.${GH} "install\\win\\install.iss"

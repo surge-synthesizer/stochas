eee989c87a961df511e28fb68479179fa7079d37
Paul Walker <paul@pwjw.com>
2024-10-27 15:13:38 -0400

    Yet more moving stuff

--- scripts/mac-build.sh
+++ scripts/mac-build.sh
@@ -12,13 +12,13 @@ cmake --build ignore/build --target stochas_CLAP --config Release --parallel
 cmake --build ignore/build --target stochas_Standalone --config Release --parallel
 
 mkdir -p ignore/mac_assets
-cp -r ignore/build/stochas_artefacts/VST3/* ignore/mac_assets
-cp -r ignore/build/stochas_artefacts/CLAP/* ignore/mac_assets
-cp -r ignore/build/stochas_artefacts/Standalone/* ignore/mac_assets
+cp -r ignore/build/stochas_artefacts/Release/VST3/* ignore/mac_assets
+cp -r ignore/build/stochas_artefacts/Release/CLAP/* ignore/mac_assets
+cp -r ignore/build/stochas_artefacts/Release/Standalone/* ignore/mac_assets
 
 cmake -GNinja -Bignore/build_au -DCMAKE_BUILD_TYPE=Release -DSTOCHAS_IS_SYNTH=FALSE -DSTOCHAS_VERSION=${STOCHAS_VERSION} -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
 cmake --build ignore/build_au --target stochas_AU --config Release --parallel
-cp -r ignore/build_au/stochas_artefacts/AU/* ignore/mac_assets
+cp -r ignore/build_au/stochas_artefacts/Release/AU/* ignore/mac_assets
 
 mkdir -p ignore/mac_installer
 lib/sst-plugininfra/scripts/installer_mac/make_installer.sh Stochas ignore/mac_assets resources ignore/mac_installer ${STOCHAS_VERSION}
